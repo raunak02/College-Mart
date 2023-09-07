0ed25caa67c85e7b93b2dc7a825d419a8aacf057
abhinav3323 <76875664+abhinav3323@users.noreply.github.com>
2023-09-07 21:57:11 +0530

    changes in the about us page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -14,12 +14,7 @@ const About = () => {
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+            College Mart is a prototype of our college's second-hand product e-commerce website. Users can sell their products via admin, and College Mart will handle all the unnecessary stress of selling on their behalf.
           </p>
         </div>
       </div>
